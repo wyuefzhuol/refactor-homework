5be3e3686d8aa66fb53293d2ef60ef490374af44
wyuefzhuol <1527169088@qq.com>
2020-08-27 20:19:25 +0800

    [refactor homework][Eric] test: given voyage zone east-indies length 3 when voyage risk then return 5

--- test/rankTest.js
+++ test/rankTest.js
@@ -12,3 +12,15 @@ rankTest('Test1: given voyage zone china length 3 when voyage risk then return 5
   //then
   t.is(result, 5);
 });
+
+rankTest('Test2: given voyage zone east-indies length 3 when voyage risk then return 5', t => {
+  //given
+  let voyage = {
+    zone: 'east-indies',
+    length: 3,
+  }
+  //when
+  let result = voyageRisk(voyage);
+  //then
+  t.is(result, 5);
+});
