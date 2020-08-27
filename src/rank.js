6712d37a629d4adebd7b887c25ba59692918b121
wyuefzhuol <1527169088@qq.com>
2020-08-27 21:49:11 +0800

    [refactor homework][Eric] refactor: extract method calcResultWhenZoneIsChinaAndHasChina and calcResultWhenZoneNotChinaOrNotHasChina

--- src/rank.js
+++ src/rank.js
@@ -1,6 +1,6 @@
 function voyageRisk (voyage) {
   let result = 1;
-  result = voyage.length > 4 ? (voyage.length > 8 ? result + voyage.length - 6 : result + 2) : result;
+  result += voyage.length > 4 ? (voyage.length > 8 ? voyage.length - 6 : 2) : 0;
   result = [ 'china', 'east-indies' ].includes(voyage.zone) ? result + 4 : result;
   return Math.max(result, 0);
 }
@@ -11,10 +11,34 @@ function hasChina (history) {
 
 function captainHistoryRisk (voyage, history) {
   let result = 1;
-  result = history.length < 5 ? result + 4 : result;
+  result += history.length < 5 ? 4 : 0;
   result += history.filter(v => v.profit < 0).length;
-  result = voyage.zone === 'china' && hasChina(history) ? result - 2 : result;
+  result += voyage.zone === 'china' && hasChina(history) ? -2 : 0;
   return Math.max(result, 0);
+}
+
+function calcResultWhenZoneIsChinaAndHasChina(result, history, voyage) {
+  result += 3;
+  if (history.length > 10) {
+    result += 1;
+  }
+  if (voyage.length > 12) {
+    result += 1;
+  }
+  if (voyage.length > 18) {
+    result -= 1;
+  }
+  return result;
+}
+
+function calcResultWhenZoneNotChinaOrNotHasChina(history, result, voyage) {
+  if (history.length > 8) {
+    result += 1;
+  }
+  if (voyage.length > 14) {
+    result -= 1;
+  }
+  return result;
 }
 
 function voyageProfitFactor (voyage, history) {
@@ -26,24 +50,10 @@ function voyageProfitFactor (voyage, history) {
     result += 1;
   }
   if (voyage.zone === 'china' && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) {
-      result += 1;
-    }
-    if (voyage.length > 12) {
-      result += 1;
-    }
-    if (voyage.length > 18) {
-      result -= 1;
-    }
+    result = calcResultWhenZoneIsChinaAndHasChina(result, history, voyage);
   }
   else {
-    if (history.length > 8) {
-      result += 1;
-    }
-    if (voyage.length > 14) {
-      result -= 1;
-    }
+    result = calcResultWhenZoneNotChinaOrNotHasChina(history, result, voyage);
   }
   return result;
 }
