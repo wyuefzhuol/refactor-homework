f81e27ca09d7dc6fdcc3c0e11b6185c11238b160
wyuefzhuol <1527169088@qq.com>
2020-08-27 21:42:18 +0800

    [refactor homework][Eric] refactor: modify the if in method captainHistoryRisk to ternary expression

--- src/rank.js
+++ src/rank.js
@@ -1,7 +1,7 @@
 function voyageRisk (voyage) {
   let result = 1;
   result = voyage.length > 4 ? (voyage.length > 8 ? result + voyage.length - 6 : result + 2) : result;
-  result = [ 'china', 'east-indies' ].includes(voyage.zone) ? result + 4 : result
+  result = [ 'china', 'east-indies' ].includes(voyage.zone) ? result + 4 : result;
   return Math.max(result, 0);
 }
 
@@ -11,13 +11,9 @@ function hasChina (history) {
 
 function captainHistoryRisk (voyage, history) {
   let result = 1;
-  if (history.length < 5) {
-    result += 4;
-  }
+  result = history.length < 5 ? result + 4 : result;
   result += history.filter(v => v.profit < 0).length;
-  if (voyage.zone === 'china' && hasChina(history)) {
-    result -= 2;
-  }
+  result = voyage.zone === 'china' && hasChina(history) ? result - 2 : result;
   return Math.max(result, 0);
 }
 
