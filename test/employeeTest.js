481a122bd7373b336563761d5547ae9fd8b9debd
wyuefzhuol <1527169088@qq.com>
2020-08-28 11:37:08 +0800

    [refactor homework][Eric] refactor: given name eric type doctor when new employee then return Employee cannot be of type doctor

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -10,3 +10,17 @@ employeeTest('Test1: given name eric type engineer when new employee then return
   //then
   t.is(result.toString(), "eric (engineer)");
 });
+
+employeeTest('Test2: given name eric type doctor when new employee then return Employee cannot be of type doctor', t => {
+  //given
+  let name = "eric";
+  let type = "doctor"
+  try {
+    //when
+    const result = new Employee(name, type);
+    t.fail();
+  } catch (e) {
+    //then
+    t.is(e.message, "Employee cannot be of type doctor");
+  }
+});
