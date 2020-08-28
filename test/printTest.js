084830961b8c350c15bffc7db2bdf8d7c35ec9fa
wyuefzhuol <1527169088@qq.com>
2020-08-28 11:58:23 +0800

    [refactor homework][Eric] test: given given invoice customer eric no border spacing when print owing then return amount info

--- test/printTest.js
+++ test/printTest.js
@@ -1,7 +1,7 @@
 const printTest = require('ava');
 const {printOwing} = require('../src/print');
 
-printTest('Test1: given invoice customer eric when new employee then return amount info', t => {
+printTest('Test1: given invoice customer eric when print owing then return amount info', t => {
   //given
   let invoice = {
     customer: "eric",
@@ -13,5 +13,20 @@ printTest('Test1: given invoice customer eric when new employee then return amou
   //then
   t.is(result, "***********************\n**** Customer Owes ****\n***********************\n" +
     "name: eric\namount: 15\namount: " +
+    (new Date((new Date).getFullYear(), (new Date).getMonth(), (new Date).getDate() + 30)).toLocaleDateString());
+});
+
+printTest('Test2: given invoice customer eric no border spacing when print owing then return amount info', t => {
+  //given
+  let invoice = {
+    customer: "eric",
+    borderSpacing: [],
+    dueDate: null
+  }
+  //when
+  const result = printOwing(invoice);
+  //then
+  t.is(result, "***********************\n**** Customer Owes ****\n***********************\n" +
+    "name: eric\namount: 0\namount: " +
     (new Date((new Date).getFullYear(), (new Date).getMonth(), (new Date).getDate() + 30)).toLocaleDateString());
 });
