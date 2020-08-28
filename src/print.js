e1e57b655243bf5b536d8dbdce4402e480908fe1
wyuefzhuol <1527169088@qq.com>
2020-08-28 12:29:49 +0800

    [refactor homework][Eric] refactor: extract the method recordDueDate in printOwing

--- src/print.js
+++ src/print.js
@@ -6,15 +6,19 @@ function calcOutstanding(invoice, outstanding) {
   return outstanding;
 }
 
+function recordDueDate(invoice) {
+  // record due date
+  const today = new Date();
+  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
 function printOwing (invoice) {
   let outstanding = 0;
   let result = '***********************\n**** Customer Owes ****\n***********************\n';
 
   outstanding = calcOutstanding(invoice, outstanding);
 
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+  recordDueDate(invoice);
 
   // print details
   result += `name: ${invoice.customer}\namount: ${outstanding}\namount: ${invoice.dueDate.toLocaleDateString()}`
