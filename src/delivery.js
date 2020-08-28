7e73109e4f2103c10f4f7fe4610efe664745d5db
wyuefzhuol <1527169088@qq.com>
2020-08-28 17:40:37 +0800

    [refactor homework][Eric] refactor: extract the const ME,NH

--- src/delivery.js
+++ src/delivery.js
@@ -2,50 +2,50 @@ function isInclude(deliveryStates, actualDeliveryState) {
   return deliveryStates.includes(actualDeliveryState);
 }
 
-//不太清除这些是什么状态，因此没有合适的命名
-const state1 = [
-  'MA',
-  'CT',
-];
-
-function deliveryDate (anOrder, isRush) {
+function deliveryDate(anOrder, isRush) {
   if (isRush) {
     let deliveryTime;
     if (isInclude(state1, anOrder.deliveryState)) {
       deliveryTime = 1;
-    }
-    else if (isInclude([
-      'NY',
-      'NH',
-    ], anOrder.deliveryState)) {
+    } else if (isInclude(state2, anOrder.deliveryState)) {
       deliveryTime = 2;
-    }
-    else {
+    } else {
       deliveryTime = 3;
     }
     return anOrder.placedOn.plusDays(1 + deliveryTime);
-  }
-  else {
+  } else {
     let deliveryTime;
-    if (isInclude([
-      'MA',
-      'CT',
-      'NY',
-    ], anOrder.deliveryState)) {
+    if (isInclude(state3, anOrder.deliveryState)) {
       deliveryTime = 2;
-    }
-    else if (isInclude([
-      'ME',
-      'NH',
-    ], anOrder.deliveryState)) {
+    } else if (isInclude(state4, anOrder.deliveryState)) {
       deliveryTime = 3;
-    }
-    else {
+    } else {
       deliveryTime = 4;
     }
     return anOrder.placedOn.plusDays(2 + deliveryTime);
   }
 }
+
+//不太清除这些是什么状态，因此没有合适的命名
+const state1 = [
+  'MA',
+  'CT',
+];
+//不太清除这些是什么状态，因此没有合适的命名
+const state2 = [
+  'NY',
+  'NH',
+];
+//不太清除这些是什么状态，因此没有合适的命名
+const state3 = [
+  'MA',
+  'CT',
+  'NY',
+];
+const state4 = [
+  'ME',
+  'NH',
+];
 
 module.exports = {
   deliveryDate
